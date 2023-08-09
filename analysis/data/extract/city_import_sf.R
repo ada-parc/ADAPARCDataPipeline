c5ac65f632f4f97848556168c26cb25600d1f00e
janehuber <59630505+janehuber@users.noreply.github.com>
2023-08-09 15:17:21 -0500

    Use the correct FIPS code objects

--- analysis/data/extract/city_import_sf.R
+++ analysis/data/extract/city_import_sf.R
@@ -6,15 +6,17 @@ rm(config_values)
 
 # Get FIPS
 
-fips_codes_for_lookup <- getFIPSCodes()
+fips_codes_for_state_and_county <- getFIPSCodesForStateAndCounty()
+
+fips_codes_for_states <- getFIPSCodesForStates()
 
 # Places/Cities lookup ----------------------------------------------------
 
-places_sf <- downloadSFOfCitiesPlaces(year, fips_codes_for_lookup)
+places_sf <- downloadSFOfCitiesPlaces(year, fips_codes_for_states)
 
-counties_sf <- downloadSFOfCounties(year, fips_codes_for_lookup)
+counties_sf <- downloadSFOfCounties(year, fips_codes_for_states)
 
-tracts_sf <- downloadSFOfTracts(year, fips_codes_for_lookup)
+tracts_sf <- downloadSFOfTracts(year, fips_codes_for_state_and_county)
 
 rm(fips_codes_for_lookup)
 
