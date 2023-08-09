7e3f15d3b29c8ffc9c082098c4d247cba42037b0
janehuber <59630505+janehuber@users.noreply.github.com>
2023-08-09 15:28:35 -0500

    Update fips codes func documentation

--- R/getFIPSCodesForStateAndCounty.R
+++ R/getFIPSCodesForStateAndCounty.R
@@ -1,19 +1,19 @@
-#' getFIPSCodes
+#' getFIPSCodesForStateAndCounty
 #'
-#' Downloads FIPS codes to use for analysis
+#' Downloads FIPS codes for states and counties to use for analysis
 #'
-#' @returns A df including information on US FIPS codes, filtered for use in analysis
+#' @returns A df of US FIPS codes for states and counties, filtered for use in analysis. Note that state codes are unique and county codes are unique WITHIN states.
 #'
 #' @import dplyr
 #' @import tigris
 #'
 
-getFIPSCodes <- function() {
+getFIPSCodesForStateAndCounty <- function() {
 
   fips_codes_for_lookup <- c(
       tigris::fips_codes %>%
         dplyr::filter(as.numeric(state_code) %in% c(1:56, 72)) %>% # Remove US Outlying Islands/US Virgin Islands (c(1:56, 72))
-        dplyr::select(state_code) %>%
+        dplyr::select(state_code, county_code) %>%
         dplyr::distinct()
     )
 
