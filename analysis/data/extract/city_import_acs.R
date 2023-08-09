e042d1524ca0197929bd58873fd4693db33d3714
janehuber <59630505+janehuber@users.noreply.github.com>
2023-08-09 15:18:22 -0500

    Use the correct fips codes for city acs

--- analysis/data/extract/city_import_acs.R
+++ analysis/data/extract/city_import_acs.R
@@ -1,18 +1,16 @@
+# Get year config
 year <- config::get("acs")$years[1]
 
+# Places data download
 tables <- c("S1810", "S1811")
-
 places_acs <- downloadACSPlace(tables, "place", year, "acs5")
 
-
-# MOVE CITY_PLACE_FULL to one of the transform sections; not referenced until loading flows (where we're exporting Rda's)
-# city_place_full <- places_acs %>%
-#   filter(GEOID %in% places_sf$place_GEOID)
-
+# Tracts data download
 tables <- c("S1810")
-tracts_data <- downloadACSTracts(tables, "tracts", year, getFIPSCodes())
+tracts_data <- downloadACSTracts(tables, "tracts", year, getFIPSCodesForStateAndCounty())
 
 
+rm("tables")
 save.image(here::here("city", "import_acs", "output", "city_import_acs.Rda"))
 
 rm(list = ls())
