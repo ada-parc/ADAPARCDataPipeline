75ef29d67ed7ae872c12959129a13e8f6cb7f6bc
janehuber <59630505+janehuber@users.noreply.github.com>
2023-08-25 18:09:17 -0500

    Add note to improve getting base to source map

--- R/getADAPARCBaseToSourceVariableMap.R
+++ R/getADAPARCBaseToSourceVariableMap.R
@@ -9,5 +9,7 @@
 
 getADAPARCBaseToSourceVariableMap <- function() {
 
+  # TODO: Check for errors--for example, need unique ada_parc_var_names and unique source_var_names for each year (i.e. no repeats within a specified year)
+
   return(readxl::read_xlsx(here::here("analysis", "data", "ada_parc_base_variables_mapped_to_source_variables.xlsx")))
 }
