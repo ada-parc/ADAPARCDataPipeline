9a166f81c50f1cc5c04db432720f5fc9315dc408
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-01 11:12:13 -0500

    Update vars in calculations for participation

--- R/addCalculatedVariablesForBaseParticipation.R
+++ R/addCalculatedVariablesForBaseParticipation.R
@@ -27,9 +27,9 @@ addCalculatedVariablesForBaseParticipation <- function(base_data) {
 
         ### ----- CP. Pop, PWD, PWOD -----
         # Pop
-        pop_total = S1810_C01_001_estimate,
+        pop_total = pop_total,
         # PWD
-        pwd_total = S1810_C02_001_estimate,
+        pwd_total = pwd_total,
         pwd_pct = pwd_total / pop_total,
         # PWOD
         pwod_total = pop_total - pwd_total,
@@ -38,33 +38,33 @@ addCalculatedVariablesForBaseParticipation <- function(base_data) {
         ### ----- CP. Health Insurance -----
 
         ### ----- CP. Insured/Uninsured, 19-64 -----
-        pop_19_64 = B18135_013_estimate,
-        pwd_19_64 = B18135_014_estimate,
-        pwd_19_64_insured = B18135_015_estimate,
-        pwd_19_64_insured_private = B18135_016_estimate,
-        pwd_19_64_insured_public = B18135_017_estimate,
-        pwd_19_64_uninsured = B18135_018_estimate,
+        pop_19_64 = pop_19_64,
+        pwd_19_64 = pwd_19_64,
+        pwd_19_64_insured = pwd_19_64_insured,
+        pwd_19_64_insured_private = pwd_19_64_insured_private,
+        pwd_19_64_insured_public = pwd_19_64_insured_public,
+        pwd_19_64_uninsured = pwd_19_64_uninsured,
 
-        pwod_19_64 = B18135_019_estimate,
-        pwod_19_64_insured = B18135_020_estimate,
-        pwod_19_64_insured_private = B18135_021_estimate,
-        pwod_19_64_insured_public = B18135_022_estimate,
-        pwod_19_64_uninsured = B18135_023_estimate,
+        pwod_19_64 = pwod_19_64,
+        pwod_19_64_insured = pwod_19_64_insured,
+        pwod_19_64_insured_private = pwod_19_64_insured_private,
+        pwod_19_64_insured_public = pwod_19_64_insured_public,
+        pwod_19_64_uninsured = pwod_19_64_uninsured,
 
         ### ----- CP. Insured/Uninsured, 65+ -----
-        pop_grtoeq_65 = B18135_024_estimate,
+        pop_grtoeq_65 = pop_grtoeq_65,
 
-        pwd_grtoeq_65 = B18135_025_estimate,
-        pwd_grtoeq_65_insured = B18135_026_estimate,
-        pwd_grtoeq_65_insured_private = B18135_027_estimate,
-        pwd_grtoeq_65_insured_public = B18135_028_estimate,
-        pwd_grtoeq_65_uninsured = B18135_029_estimate,
+        pwd_grtoeq_65 = pwd_grtoeq_65,
+        pwd_grtoeq_65_insured = pwd_grtoeq_65_insured,
+        pwd_grtoeq_65_insured_private = pwd_grtoeq_65_insured_private,
+        pwd_grtoeq_65_insured_public = pwd_grtoeq_65_insured_public,
+        pwd_grtoeq_65_uninsured = pwd_grtoeq_65_uninsured,
 
-        pwod_grtoeq_65 = B18135_030_estimate,
-        pwod_grtoeq_65_insured = B18135_031_estimate,
-        pwod_grtoeq_65_insured_private = B18135_032_estimate,
-        pwod_grtoeq_65_insured_public = B18135_033_estimate,
-        pwod_grtoeq_65_uninsured = B18135_034_estimate,
+        pwod_grtoeq_65 = pwod_grtoeq_65,
+        pwod_grtoeq_65_insured = pwod_grtoeq_65_insured,
+        pwod_grtoeq_65_insured_private = pwod_grtoeq_65_insured_private,
+        pwod_grtoeq_65_insured_public = pwod_grtoeq_65_insured_public,
+        pwod_grtoeq_65_uninsured = pwod_grtoeq_65_uninsured,
 
         ### ----- CP. Insurance, Percents -----
         pwd_19_64_insured_pct = pwd_19_64_insured / pwd_19_64,
@@ -91,44 +91,44 @@ addCalculatedVariablesForBaseParticipation <- function(base_data) {
         ### ----- CP. Commute to Work -----
         ## Transit
         # Population
-        pop_commute_public_pct = S1811_C01_035_estimate / 100,
-        pop_commute_public = pop_commute_public_pct * S1811_C01_032_estimate,
+        pop_commute_public_pct = pop_commute_public_pct / 100,
+        pop_commute_public = pop_commute_public_pct * pop_total_commute,
         # PWD
-        pwd_commute_public_pct = S1811_C02_035_estimate / 100,
-        pwd_commute_public = pwd_commute_public_pct * S1811_C02_032_estimate,
+        pwd_commute_public_pct = pwd_commute_public_pct / 100,
+        pwd_commute_public = pwd_commute_public_pct * pwd_total_commute,
         # PWOD
-        pwod_commute_public_pct = S1811_C03_035_estimate / 100,
-        pwod_commute_public = pwod_commute_public_pct * S1811_C03_032_estimate,
+        pwod_commute_public_pct = pwod_commute_public_pct / 100,
+        pwod_commute_public = pwod_commute_public_pct * pwod_total_commute,
         ## Private Car
         # Population
-        pop_commute_car_alone_pct = S1811_C01_033_estimate / 100,
-        pop_commute_car_alone = pop_commute_car_alone_pct * S1811_C01_032_estimate,
+        pop_commute_car_alone_pct = pop_commute_car_alone_pct / 100,
+        pop_commute_car_alone = pop_commute_car_alone_pct * pop_total_commute,
         # PWD
-        pwd_commute_car_alone_pct = S1811_C02_033_estimate / 100,
-        pwd_commute_car_alone = pwd_commute_car_alone_pct * S1811_C02_032_estimate,
+        pwd_commute_car_alone_pct = pwd_commute_car_alone_pct / 100,
+        pwd_commute_car_alone = pwd_commute_car_alone_pct * pwd_total_commute,
         # PWOD
-        pwod_commute_car_alone_pct = S1811_C03_033_estimate / 100,
-        pwod_commute_car_alone = pwod_commute_car_alone_pct * S1811_C03_032_estimate,
+        pwod_commute_car_alone_pct = pwod_commute_car_alone_pct / 100,
+        pwod_commute_car_alone = pwod_commute_car_alone_pct * pwod_total_commute,
 
         ### ----- CP. Educational Attainment -----
         ### Percents
-        pwd_lessthan_highschool_pct = S1811_C02_040_estimate / 100,
-        pwod_lessthan_highschool_pct = S1811_C03_040_estimate / 100,
-        pwd_highschoolequiv_pct = S1811_C02_041_estimate / 100,
-        pwod_highschoolequiv_pct = S1811_C03_041_estimate / 100,
-        pwd_degree_aa_pct = S1811_C02_042_estimate / 100,
-        pwod_degree_aa_pct = S1811_C03_042_estimate / 100,
-        pwd_degree_grtoeq_ba_pct = S1811_C02_043_estimate / 100,
-        pwod_degree_grtoeq_ba_pct = S1811_C03_043_estimate / 100,
+        pwd_lessthan_highschool_pct = pwd_lessthan_highschool_pct / 100,
+        pwod_lessthan_highschool_pct = pwod_lessthan_highschool_pct / 100,
+        pwd_highschoolequiv_pct = pwd_highschoolequiv_pct / 100,
+        pwod_highschoolequiv_pct = pwod_highschoolequiv_pct / 100,
+        pwd_degree_aa_pct = pwd_degree_aa_pct / 100,
+        pwod_degree_aa_pct = pwod_degree_aa_pct / 100,
+        pwd_degree_grtoeq_ba_pct = pwd_degree_grtoeq_ba_pct / 100,
+        pwod_degree_grtoeq_ba_pct = pwod_degree_grtoeq_ba_pct / 100,
         ### Values (base below named "pwd_pop_educ" in map)
-        pwd_lessthan_highschool = pwd_lessthan_highschool_pct * S1811_C02_039_estimate,
-        pwod_lessthan_highschool = pwod_lessthan_highschool_pct * S1811_C03_039_estimate,
-        pwd_highschoolequiv = pwd_highschoolequiv_pct * S1811_C02_039_estimate,
-        pwod_highschoolequiv = pwod_highschoolequiv_pct * S1811_C03_039_estimate,
-        pwd_degree_aa = pwd_degree_aa_pct * S1811_C02_039_estimate,
-        pwod_degree_aa = pwod_degree_aa_pct * S1811_C03_039_estimate,
-        pwd_degree_grtoeq_ba = pwd_degree_grtoeq_ba_pct * S1811_C02_039_estimate,
-        pwod_degree_grtoeq_ba = pwod_degree_grtoeq_ba_pct * S1811_C03_039_estimate
+        pwd_lessthan_highschool = pwd_lessthan_highschool_pct * pwd_pop_educ,
+        pwod_lessthan_highschool = pwod_lessthan_highschool_pct * pwod_pop_educ,
+        pwd_highschoolequiv = pwd_highschoolequiv_pct * pwd_pop_educ,
+        pwod_highschoolequiv = pwod_highschoolequiv_pct * pwod_pop_educ,
+        pwd_degree_aa = pwd_degree_aa_pct * pwd_pop_educ,
+        pwod_degree_aa = pwod_degree_aa_pct * pwod_pop_educ,
+        pwd_degree_grtoeq_ba = pwd_degree_grtoeq_ba_pct * pwd_pop_educ,
+        pwod_degree_grtoeq_ba = pwod_degree_grtoeq_ba_pct * pwod_pop_educ
       ) %>%
       mutate(across(.cols = ends_with("pct"),.fns = ~ round(.x * 100, 2)))
 
