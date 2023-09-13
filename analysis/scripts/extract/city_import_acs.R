314cc30d81fe334e6aded30c714e0fcde61b85b3
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-13 17:02:52 -0500

    Get multiple years of city acs; use standard funcs

--- analysis/scripts/extract/city_import_acs.R
+++ analysis/scripts/extract/city_import_acs.R
@@ -1,21 +1,23 @@
 # Get year config
-year <- config::get("acs")$years[1]
+years <- config::get("acs")$years
 survey <- config::get("acs")$survey
+
+fips_for_state <- getFIPSCodesForStates()
 
 # Places data download
 #TODO: This function is too specific... It's likely that the direction extraction will go in is to only pull variables that we specifically want, in which case this might fit in with that flow.
 places_pop_est <- downloadACSPlacePopulationEstimate(year)
 
 tables <- c("S1810", "S1811")
-places_acs <- downloadCountryWideAcs(tables, "place", year, survey)
+places_data_acs_raw <- getCountryWideAcsTablesForMultipleYears(tables, years, survey, "place")
 
 
 # Tracts data download
 tables <- c("S1810")
-fips_for_state <- getFIPSCodesForStates()$state_code
+tracts_data_acs_raw <- getCountryWideAcsTablesForMultipleYears(tables, years, survey, "tract", fips_for_state)
 
-tracts_data <- downloadACSTracts(tables, "tract", year, survey, fips_for_state)
+
 
 saveRawExtractedDataFile(places_pop_est, "places_pop_est")
-saveRawExtractedDataFile(places_acs, "places_acs")
-saveRawExtractedDataFile(tracts_data, "tracts_data")
+saveRawExtractedDataFile(places_data_acs_raw, "places_data_acs_raw")
+saveRawExtractedDataFile(tracts_data_acs_raw, "tracts_data_acs_raw")
