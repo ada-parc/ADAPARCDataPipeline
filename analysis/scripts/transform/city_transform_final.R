2eb5f64615bcb795b5cc606d62e38090be7025a3
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-13 21:09:03 -0500

    Set up final tract_data

--- analysis/scripts/transform/city_transform_final.R
+++ analysis/scripts/transform/city_transform_final.R
@@ -1,4 +1,4 @@
-# Script to combine city data from different sources or final transformations
+# Script to combine city data from different sources or final transformations that don't make sense in a source-specific transformation script
 
 ## NOTE: Until the website code is updated, do not change the final file names--ultimately, we should change them to follow our expected pattern, but it will be easier to transition the website code if we know things work with the old names first.
 
@@ -6,7 +6,9 @@ city_place_data_final <- readTransformedExtractedDataFile("city_place_data_proce
 saveFinalDataFile(city_place_data_processed, "city_place_full")
 
 
+city_tracts_data_final <- readTransformedExtractedDataFile("city_tracts_data_processed")
+saveFinalDataFile(city_place_data_processed, "tract_data")
+
+
 # TODO: Fill this in
 # dict_location_crosswalk
-#
-# tracts_data
