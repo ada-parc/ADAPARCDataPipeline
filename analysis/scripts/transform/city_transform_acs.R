b49b7a247025ce620d816a6d9376d5bcd5333ebc
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-14 14:56:33 -0500

    Normalize city_place and city_tract data
    Use the pattern we have established to get filtered data, convert variables to our base variables, and then produce a processed version of the data

--- analysis/scripts/transform/city_transform_acs.R
+++ analysis/scripts/transform/city_transform_acs.R
@@ -14,13 +14,17 @@ ada_parc_base_variable_map <- getADAPARCBaseToSourceVariableMap(years)
 place_geoids_in_scope <- unique(places_sf$place_GEOID)
 city_place_data_filtered <- filterPlaceAcsDataToScope(places_data_acs_raw, place_geoids_in_scope)
 
-city_place_data_processed <-  transformRawVariablesToADAPARCBaseVariables(ada_parc_base_variable_map, city_place_data_filtered, "acs")
+city_place_data_base <-  transformRawVariablesToADAPARCBaseVariables(ada_parc_base_variable_map, city_place_data_filtered, "acs")
+
+city_place_data_processed <- addCalculatedVariablesForBaseDemographics(city_place_data_base)
 
 saveTransformedDataFile(city_place_data_processed, "city_place_data_processed")
 
 # Create "tracts_data" for the website:
 # No filtering needs to happen; the data used to be requested according to the state and county names present in the "places_counties" df, but the same data is retrieved without doing any filtering.
 
-city_tracts_data_processed <- transformRawVariablesToADAPARCBaseVariables(ada_parc_base_variable_map, tracts_data_acs_raw, "acs")
+city_tracts_data_base <- transformRawVariablesToADAPARCBaseVariables(ada_parc_base_variable_map, tracts_data_acs_raw, "acs")
+
+city_tracts_data_processed <- addCalculatedVariablesForBaseDemographics(city_tracts_data_base)
 
 saveTransformedDataFile(city_tracts_data_processed, "city_tracts_data_processed")
