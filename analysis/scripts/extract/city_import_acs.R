95f8cad2a510398fa6d8611a38ab20e275de0109
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-13 20:30:16 -0500

    Update thinking about places_pop_est
    I'm not sure that I'm using this exactly how I want just yet...

--- analysis/scripts/extract/city_import_acs.R
+++ analysis/scripts/extract/city_import_acs.R
@@ -6,7 +6,8 @@ fips_for_state <- getFIPSCodesForStates()
 
 # Places data download
 #TODO: This function is too specific... It's likely that the direction extraction will go in is to only pull variables that we specifically want, in which case this might fit in with that flow.
-places_pop_est <- downloadACSPlacePopulationEstimate(year)
+# We use this later in order to filter out places with small populations; it is acceptable to pull this data for only the most recent year to filter places, since that's likely what we're most interested in. Was uncertain about this decision or not, so if someone else feels strongly, please change.
+places_pop_est <- downloadACSPlacePopulationEstimate(years[2])
 
 tables <- c("S1810", "S1811")
 places_data_acs_raw <- getCountryWideAcsTablesForMultipleYears(tables, years, survey, "place")
