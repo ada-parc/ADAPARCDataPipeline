ef29cf5916e3bf07b7a456a6d8087a563b999e75
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-19 17:00:57 -0500

    Update the pipes to access proper input parameter

--- R/splitPlacesSFByState.R
+++ R/splitPlacesSFByState.R
@@ -1,6 +1,6 @@
 #' splitPlacesSFByState
 #'
-#' Split a Places SF dataframe into a split dataframe according to states. Note: "Places" is a geography defined by the US Census.
+#' Split a Places SF dataframe into a split dataframe according to states. Note: "Places" is a geography defined by the US Census. We filter according to population that is 3500+
 #'
 #' @param places original dataframe of places SF
 #' @param places_pop_est dataframe containing places and their population
@@ -12,12 +12,12 @@
 splitPlacesSFByState <- function(places, places_pop_est) {
 
   filtered_places <-
-    places_sf %>%
+    places %>%
     dplyr::mutate(STATEFP = as.factor(STATEFP)) %>%
     dplyr::left_join(places_pop_est, by = c("place_GEOID" = "GEOID")) %>%
     dplyr::filter(POP > 3500)
 
   places_split <- split(filtered_places, filtered_places$STATEFP)
 
-
+  return(places_split)
 }
