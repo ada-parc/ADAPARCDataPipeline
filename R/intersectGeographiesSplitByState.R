3402a859b8b1192e6db77a38f811f1eab5b181d8
janehuber <59630505+janehuber@users.noreply.github.com>
2023-09-20 12:06:44 -0500

    Update intersectGeographiesSplitByState
    Will want to rename this, since it ultimately produces a map linking places with tracts. That said, it works right now and is dramatically more efficient than its previous version (~95% performance improvement)

--- R/intersectGeographiesSplitByState.R
+++ R/intersectGeographiesSplitByState.R
@@ -1,36 +1,97 @@
 #' intersectGeographiesSplitByState
 #'
-#' Intersect two SF dataframes that are split by states
+#' Intersect two SF dataframes that are split by states. They will be intersected, left-right. The two dataframes must be of the same length.
 #'
 #'
-#' @param geography_split_by_state_1 a dataframe (type SF) that is split by state
-#' @param geography_split_by_state_2 a dataframe (type SF) that is split by state
+#' @param places_sf a dataframe (type SF) that is split by state
+#' @param tracts_sf a dataframe (type SF) that is split by state
 #'
 #' @returns a dataframe that combines
 #'
 #'
 #' @import dplyr
+#' @import purrr
+#' @import sf
 
-intersectGeographiesSplitByState <- function(geography_split_by_state_1,
-                                       geography_split_by_state_2) {
+intersectGeographiesSplitByState <- function(places_sf,
+                                       tracts_sf) {
 
-  combined_dfs <-
-    dplyr::map(1:52, function(x) {
+  map_min <- 1
+  map_max <- length(places_sf)
 
-      message(paste("Running Intersection", x, "of 52"))
-      df <-
-        sf::st_intersection(geography_split_by_state_1[[x]], geography_split_by_state_2[[x]])
-      gc()
-      return(df)
-    })
+  intersect_geographies_by_state <- function(index) {
 
-  combined_geographies <- combined_dfs %>%
-    purrr::reduce(dplyr::bind_rows) %>%
-    dplyr::mutate("overlap_area" = st_area(.),
-           "overlap_area_num" = as.numeric(overlap_area),
-           "overlap_pct" = overlap_area_num / place_area_num) %>%
-    dplyr::filter(overlap_pct > 0) %>%
-    sf::st_drop_geometry()
+    message(paste("Running Intersection ", index, " of ", map_max))
+
+    places_sf_state <- places_sf[[index]]
+    tract_sf_state <- tracts_sf[[index]]
+
+    # sf::st_intersects is important because it is a relatively efficient operation to determine TRUE/FALSE if geographies overlap. Note that some of these intersections can be two borders that are touching and so we call them "potential"
+    potential_intersections <- sf::st_intersects(places_sf_state, tract_sf_state)
+
+
+    result <-
+      data.frame(
+        place_GEOID = character(0),
+        place_NAME = character(0),
+        tract_GEOID = character(0),
+        tract_NAME = character(0),
+        STATEFP = character(0),
+        COUNTYFP = character(0)
+      )
+
+
+    # Iterate through potential intersections, using the places as the starting point. For each place, we look at each census tract that was marked "TRUE" by st_intersects.
+    for (i in seq_along(potential_intersections)) {
+
+
+      place_observation <- places_sf_state[i, ]
+
+      # These are the census tract indices that are potentially in-scope for each place:
+      intersection_indices <- potential_intersections[[i]]
+
+
+      for (j in intersection_indices) {
+        tract_observation <- tract_sf_state[j, ]
+
+        # Because the tract may border the place instead of actually overlapping it, we
+        overlap_area <-
+          as.numeric(sf::st_area(
+            sf::st_intersection(place_observation, tract_observation)
+          ))
+
+        if (length(overlap_area) > 0 &&
+            !is.na(overlap_area) && overlap_area > 0) {
+
+
+          # We can save overlap area data here, but we don't use it. We save the other columns that we know we will want later and already have access to.
+          result <-
+            rbind(
+              result,
+              data.frame(
+                place_GEOID = place_observation$place_GEOID,
+                place_NAME = place_observation$place_NAME,
+                tract_GEOID = tract_observation$tract_GEOID,
+                tract_NAME = tract_observation$tract_NAME,
+                STATEFP = tract_observation$STATEFP,
+                COUNTYFP = tract_observation$COUNTYFP
+              )
+            )
+        }
+      }
+    }
+
+    return(result)
+
+  }
+
+
+
+  combined_geographies <-
+    purrr::map(map_min:map_max,
+               ~ intersect_geographies_by_state(.x),
+               .progress = TRUE) %>%
+    purrr::reduce(dplyr::bind_rows)
 
 
   return(combined_geographies)
