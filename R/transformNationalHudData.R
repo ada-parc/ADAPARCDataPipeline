600e5270175e0b279d0998f61892738e3fd695c8
janehuber <59630505+janehuber@users.noreply.github.com>
2023-11-15 11:15:37 -0600

    Finalize transforming nat'l hud data
    Improvements for performance by filtering before further operations. Additionally, added in a NAME column, which removes the two-digit abbreviation and space before the name of the state/territory.

--- R/transformNationalHudData.R
+++ R/transformNationalHudData.R
@@ -5,26 +5,28 @@
 #' @return transformed hud data
 #'
 #' @import dplyr
+#' @import tidyr
 transformNationalHudData <- function(hud_data) {
 
   # TODO: Update states to have their abbreviation and name as separate columns
 
   hud_data_transformed <- hud_data %>%
+    dplyr::filter(program_label %in% c("Housing Choice Vouchers", "Public Housing") &
+                  !States %in% c("GU Guam", "MP Northern Mariana Islands", "VI U.S. Virgin Islands")) %>%
     dplyr::mutate(total_pct_under62 = rowSums(select(., c(pct_lt24_head, pct_age25_50, pct_age51_61)), na.rm = TRUE),
                  total_pct_disabled = (
                    ifelse(is.na(pct_disabled_lt62), 0, pct_disabled_lt62) *
                      total_pct_under62 +
                      ifelse(is.na(pct_disabled_ge62), 0, pct_disabled_ge62 * pct_age62plus)
-                 ) %>% replace_na(0)
+                 ) %>% replace_na(0),
+                 NAME = sub("^[A-Z]{2} |^[A-Z]{2}$", "", States) # This allows for the name of the state to be extracted; will be used to join to other data later on
     ) %>%
-    dplyr::filter(program_label %in% c("Housing Choice Vouchers", "Public Housing")) %>%
-    dplyr::select(States, program_label, total_pct_disabled) %>%
-    dplyr::group_by(States, program_label) %>%
+    dplyr::select(NAME, program_label, total_pct_disabled) %>%
+    dplyr::group_by(NAME, program_label) %>%
     dplyr::summarise(total_pct_disabled = sum(ifelse(is.na(total_pct_disabled), 0, total_pct_disabled), na.rm = TRUE)) %>%
     tidyr::pivot_wider(names_from = program_label, values_from = total_pct_disabled) %>%
     dplyr::rename(pwd_housing_choice_voucher_pct = `Housing Choice Vouchers`, pwd_pubhousing_pct = `Public Housing`) %>%
-    dplyr::ungroup() %>%
-    dplyr::filter(!States %in% c("GU Guam", "MP Northern Mariana Islands", "VI U.S. Virgin Islands"))
+    dplyr::ungroup()
 
   return(hud_data_transformed)
 }
